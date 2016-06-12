a8d299b889206e3e0956cec939ea0b9a9b266caa
anddani <anddani@kth.se>
2016-06-12 10:10:05 +0200

    Synced with new functionality

--- app/assets/javascripts/bitmapfunctions.js
+++ app/assets/javascripts/bitmapfunctions.js
@@ -2,27 +2,44 @@ $(document).ready(function() {
 
 $("#description").text("jQuery works!");
 
+// The clear button empties the inputbox and removes the error
+// message
+$("#clear").click(function(){
+    $("#inputbox").val("");
+    $("#error").text("");
+});
+
+// When the translate button is clicked, the inputbox
+// will change its value to the sequence of bits
 $("#translate").unbind("click").on('click', function(){
     console.log("translate button clicked");
+
     // Remove newline and take the first 32 characters
     var userString = $("#inputbox").val().replace(/^\s+|\s+$/g, '');
     var inputArray = userString.split('\n');
-    for (var i = 0, l = inputArray.length; i < l; i++) {
-        console.log(i + ": " + inputArray[i]);
-    }
+
+    // Build string from two lines if there were
+    // two lines in the inputbox
     if (inputArray.length > 1) {
+	for (var i = 0, l = inputArray.length; i < l; i++) {
+	    inputArray[i] = inputArray[i].substring(0,16);
+	}
         var minLength = Math.min(inputArray[0].length, inputArray[1].length);
         var tempString = "";
         for (var i = 0; i < minLength; i++) {
-            tempString = tempString.concat(inputArray[0].charAt(i));
-            tempString = tempString.concat(inputArray[1].charAt(i));
+	    tempString = tempString.concat(inputArray[0].charAt(i));
+	    tempString = tempString.concat(inputArray[1].charAt(i));
         }
-        userString = tempString;
+	userString = tempString;
     }
-    console.log(userString);
+
+    // The longest format of the string is 32 characters
     userString = userString.substring(0,32);
-    console.log(userString);
+
+    // Regular expression for cheching whether the string is valid
+    // for secondary bitmap or not.
     var validRegex = /^[8-9A-F][A-F0-9]{31,}$|^[0-7][A-F0-9]{15,}$/;
+    console.log("size: " + userString.length);
 
     // If string is in HEX-format and at least 16 characters
     if ( validRegex.test(userString) ){
@@ -35,6 +52,7 @@ $("#translate").unbind("click").on('click', function(){
     }
 });
 
+// Button for copying the contents of the input box to the clipboard
 $("#copy").click(function() {
     console.log("copy button clicked");
     $("#description").text("copy works");
@@ -44,6 +62,7 @@ $("#copy").click(function() {
     }
 });
 
+// Converts HEX string and returns the bit sequence
 function getBitmap (text) {
     var secondaryBitmap = (hexToBinary(text.charAt(0)).charAt(0) == 1);
     console.log("secondary bitmap: " + secondaryBitmap + " firstchar: " + hexToBinary(text.charAt(0)).charAt(0));
@@ -54,6 +73,7 @@ function getBitmap (text) {
     var numIter = (secondaryBitmap == true) ? 128 : 64;
     numIter = (text.length < secondaryBitmap) ? text.length : numIter;
     console.log("numiter: " + numIter);
+    $("#debug").text(text.substring(0,numIter/4));
 
     for (var i = 0; i < numIter; i++) {
         if (binary.charAt(i) == "1") {
@@ -64,6 +84,8 @@ function getBitmap (text) {
     return returnString.substring(0,returnString.length-1);
 }
 
+// A hexadecimal character as input, returns the corresponding
+// bit value as string
 function hexToBinary(s) {
     var i, k, part, ret = '';
     var lookupTable = {
@@ -79,3 +101,4 @@ function hexToBinary(s) {
 }
 
 });
+
