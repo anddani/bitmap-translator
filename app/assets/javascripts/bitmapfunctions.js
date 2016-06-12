cc01e4bb27471409dfa44b3b5b784480c56550c2
anddani <anddani@kth.se>
2016-06-12 11:02:13 +0200

    Added cobol hex conversion

--- app/assets/javascripts/bitmapfunctions.js
+++ app/assets/javascripts/bitmapfunctions.js
@@ -4,33 +4,26 @@ $("#description").text("jQuery works!");
 
 // The clear button empties the inputbox and removes the error
 // message
-$("#clear").click(function(){
-    $("#inputbox").val("");
-    $("#error").text("");
+$(".clear").click(function(){
+    var type = $(this).attr("id").split("-")[1];
+    console.log(type);
+    $("#inputbox-" + type).val("");
+    $("#error-" + type).text("");
 });
 
 // When the translate button is clicked, the inputbox
 // will change its value to the sequence of bits
-$("#translate").unbind("click").on('click', function(){
+$("#translate").unbind("click").on('click', function() {
     console.log("translate button clicked");
 
     // Remove newline and take the first 32 characters
-    var userString = $("#inputbox").val().replace(/^\s+|\s+$/g, '');
+    var userString = $("#inputbox-translate").val().replace(/^\s+|\s+$/g, '');
     var inputArray = userString.split('\n');
 
     // Build string from two lines if there were
     // two lines in the inputbox
     if (inputArray.length > 1) {
-	for (var i = 0, l = inputArray.length; i < l; i++) {
-	    inputArray[i] = inputArray[i].substring(0,16);
-	}
-        var minLength = Math.min(inputArray[0].length, inputArray[1].length);
-        var tempString = "";
-        for (var i = 0; i < minLength; i++) {
-	    tempString = tempString.concat(inputArray[0].charAt(i));
-	    tempString = tempString.concat(inputArray[1].charAt(i));
-        }
-	userString = tempString;
+        userString = flattenStringArray(inputArray);
     }
 
     // The longest format of the string is 32 characters
@@ -43,22 +36,58 @@ $("#translate").unbind("click").on('click', function(){
 
     // If string is in HEX-format and at least 16 characters
     if ( validRegex.test(userString) ){
-        console.log("OK STRING");
-        $("#error").text("");
-        $("#inputbox").val(getBitmap(userString));
+        console.log("VALID STRING");
+        $("#error-translate").text("");
+        $("#inputbox-translate").val(getBitmap(userString));
     } else {
         console.log("BAD STRING");
-        $("#error").text("Not valid string");
+        $("#error-translate").text("Not valid string");
     }
 });
 
-// Button for copying the contents of the input box to the clipboard
-$("#copy").click(function() {
-    console.log("copy button clicked");
-    $("#description").text("copy works");
+$("#convert").unbind("click").on('click', function() {
+    console.log("Convert clicked");
+    
+    var userString = $("#inputbox-convert").val().replace(/^\s+|\s+$/g, '');
+    console.log("userString before: " + userString);
+    var inputArray = userString.split('\n');
 
-    if ( $("#inputbox").val().length > 0 ) {
-        window.prompt("Copy to clipboard: Ctrl-C", $("#inputbox").val());
+    console.log("inputarray length: " + inputArray.length);
+    if (inputArray.length > 1) {
+        userString = flattenStringArray(inputArray);
+    }
+    var positive = /^[0-9]+C$/;
+    var negative = /^[0-9]+D$/;
+    if (positive.test(userString)) {
+        console.log("POSITIVE STRING");
+
+        userString = userString.slice(0, -1);
+
+        $("#error-convert").text("");
+        $("#inputbox-convert").val(parseInt(userString, 10));
+    } else if (negative.test(userString)) {
+        console.log("NEGATIVE STRING");
+
+        userString = userString.slice(0, -1);
+
+        $("#error-convert").text("");
+        $("#inputbox-convert").val("-".concat(parseInt(userString, 10)));
+    } else {
+        console.log("BAD STRING");
+        $("#error-convert").text("Not valid string");
+    }
+    console.log("user string:" + userString);
+    
+});
+
+// Button for copying the contents of the input box to the clipboard
+$(".copy").click(function() {
+    var type = $(this).attr("id").split("-")[1];
+    console.log("copy button clicked");
+    $("#description-" + type).text("copy works");
+
+    if ( $("#inputbox-" + type).val().length > 0 ) {
+        window.prompt("Copy to clipboard: Ctrl-C", $("#inputbox-" + type).val());
     }
 });
 
@@ -73,7 +102,7 @@ function getBitmap (text) {
     var numIter = (secondaryBitmap == true) ? 128 : 64;
     numIter = (text.length < secondaryBitmap) ? text.length : numIter;
     console.log("numiter: " + numIter);
-    $("#debug").text(text.substring(0,numIter/4));
+    $("#debug-translate").text(text.substring(0,numIter/4));
 
     for (var i = 0; i < numIter; i++) {
         if (binary.charAt(i) == "1") {
@@ -82,6 +111,19 @@ function getBitmap (text) {
     };
     console.log("returnString: " + returnString);
     return returnString.substring(0,returnString.length-1);
+}
+
+function flattenStringArray(inputArray) {
+    for (var i = 0, l = inputArray.length; i < l; i++) {
+        inputArray[i] = inputArray[i].substring(0,16);
+    }
+    var minLength = Math.min(inputArray[0].length, inputArray[1].length);
+    var retString = "";
+    for (var i = 0; i < minLength; i++) {
+        retString = retString.concat(inputArray[0].charAt(i));
+        retString = retString.concat(inputArray[1].charAt(i));
+    }
+    return retString;
 }
 
 // A hexadecimal character as input, returns the corresponding
